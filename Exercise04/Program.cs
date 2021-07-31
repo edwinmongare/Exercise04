44f2ec4697938579c4df3ab7e9b6d26f997d0d53
edwinmongare <edwinmongare15@gmail.com>
2021-07-31 14:47:31 +0300

    Add project files.

---
+++ Exercise04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Collections.Generic;
+using Exercise;
+
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a list of Shapes to serialize 
+            var listOfShapes = new List<Shape>
+                {
+                new Circle { Colour = "Red", Radius = 2.5 },
+                new Rectangle { Colour = "Blue", Height = 20.0, Width = 10.0 },
+                new Circle { Colour = "Green", Radius = 8.0 },
+                new Circle { Colour = "Purple", Radius = 12.3 },
+                new Rectangle { Colour = "Blue", Height = 45.0, Width = 18.0 },
+                
+                };
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "shapesFile.xml");
+            FileStream stream = File.Create(path);
+
+            var serializerXml = new XmlSerializer(typeof(List<Shape>));
+            serializerXml.Serialize(stream, listOfShapes);
+            stream.Close();
+
+            using FileStream fileXml = File.OpenRead(path);
+
+            List<Shape> loadedShapesXml =
+              serializerXml.Deserialize(fileXml) as List<Shape>;
+            foreach (Shape item in loadedShapesXml)
+            {
+                Console.WriteLine("{0} is {1} and has an area of {2:N2}",
+                  item.GetType().Name, item.Colour, item.Area);
+            }
+        }
+    }
+}
---
+++ Exercise04/Shapes.cs
@@ -0,0 +1,78 @@
+using System.Xml.Serialization;
+
+namespace Exercise
+{
+    [XmlInclude(typeof(Rectangle))]
+    [XmlInclude(typeof(Circle))]
+    public abstract class Shape
+    {
+        [XmlAttribute("colour")]
+        public string Colour;
+        [XmlAttribute("area")]
+        public abstract double Area { get; }
+    }
+
+    [XmlInclude(typeof(Square))]
+    public class Rectangle : Shape
+    {
+        [XmlAttribute("hight")]
+        public double Height { get; set; }
+        [XmlAttribute("width")]
+        public double Width { get; set; }
+        public override double Area => Height * Width;
+        public Rectangle()
+        {
+        }
+        public Rectangle(double s)
+        {
+            this.Height = s;
+            this.Width = s;
+        }
+        public Rectangle(double s, double w)
+        {
+            this.Height = s;
+            this.Width = w;
+        }
+        public Rectangle(double s, string col)
+        {
+            this.Height = s;
+            this.Width = s;
+            this.Colour = col;
+        }
+        public Rectangle(double s, double w, string col)
+        {
+            this.Height = s;
+            this.Width = w;
+            this.Colour = col;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square() { }
+        public Square(double side)
+        {
+            this.Height = side;
+            this.Width = side;
+        }
+    }
+    public class Circle : Shape
+    {
+        [XmlAttribute("rad")]
+        public double Radius;
+        public override double Area => System.Math.PI * this.Radius * this.Radius;
+        public Circle()
+        {
+        }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public Circle(double radius, string col)
+        {
+            this.Radius = radius;
+            this.Colour = col;
+
+        }
+    }
+}
